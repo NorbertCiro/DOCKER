5db9793a37f0ebda210bca0ddf92a0975429ebfa
Norberto Salazar <norberto.salazar@susuerte.com>
2023-03-31 16:05:47 -0500

    Add project Docker in Udemy

---
+++ Docker-Udemy/cron-ticker/app.js
@@ -0,0 +1,10 @@
+const cron = require('node-cron');
+
+let times = 0;
+
+cron.schedule('1-59/5 * * * * *', () => {
+    times++;
+  console.log('Corriendo cada 5 segundos', times);
+});
+
+console.log('Inicio en image docker');
